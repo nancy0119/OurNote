c70a29648a2220be6680818a567d4152b5891d59
Samiksha Garg <gargsamiksha0306@gmail.com>
2021-08-12 13:41:40 +0530

    Modified Side Bar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import LibraryBooksIcon from '@material-ui/icons/LibraryBooks';
+import SearchIcon from '@material-ui/icons/Search';
 
 function SideBar() {
     return <div className="leftBar">
+        <LibraryBooksIcon className="sideIcon currentTab hovering" style={{ display: "block" }} />
+        <SearchIcon className="sideIcon hovering" />
     </div>
 }
 
