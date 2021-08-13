419c41e68b11f7d2a21b49c6958dbd41882c9ded
Samiksha Garg <gargsamiksha0306@gmail.com>
2021-08-13 15:20:25 +0530

    Added more features to sideBar

--- src/components/SideBarFiles/NoteBookBar.jsx
+++ src/components/SideBarFiles/NoteBookBar.jsx
@@ -1,16 +1,32 @@
 import React from "react";
 import MenuBookIcon from '@material-ui/icons/MenuBook';
+import NoteIcon from '@material-ui/icons/Note';
+import Notebooks from "./NoteBooks";
 
 function NoteBooks(props) {
 
     let visibility = props.isVisible;
     return <div style={{ display: !visibility && "none" }} className="notebooks">
-        <div className="hovering" style={{ padding: "0px 5px", width: "inherit", height: "fit-content", display: "flex" }}>
+        <div className="hovering" style={{ padding: "0px 0px 0px 5px", width: "inherit", height: "fit-content", display: "flex", borderBottom: "3px solid #f3f2f1" }}>
             <MenuBookIcon style={{ display: "inline", marginRight: "15px", marginTop: "10px" }} />
             <p style={{ display: "inline", fontWeight: "bold", marginRight: "10px" }}> My NoteBook </p>
             <img src="images/down-arrow.png" style={{ width: "12px", height: "12px", marginTop: "20px" }} />
         </div>
-    </div >
+        <div style={{ display: "flex" }}>
+            <div style={{ width: "50%", height: "73vh", borderRight: "3px solid #f3f2f1" }}>
+                <Notebooks notebookColor="red" isCurrent={true} title="Demo Notebook" />
+                <Notebooks notebookColor="green" isCurrent={false} title="Notebook - 1" />
+                <div style={{ position: "absolute", bottom: "0px" }}>
+                    <center> <p className="hovering" style={{ borderTop: "3px solid #f3f2f1", padding: "10px", width: "22vh", color: "#67217A" }}>Add Section</p></center>
+                </div>
+            </div>
+            <div style={{ width: "50%", height: "73vh" }}>
+                <div style={{ position: "absolute", bottom: "0px" }}>
+                    <center> <p className="hovering" style={{ borderTop: "3px solid #f3f2f1", padding: "10px", width: "22vh", color: "#67217A" }}>Add Page</p></center>
+                </div>
+            </div>
+        </div>
+    </div >;
 }
 
 export default NoteBooks;
